98c133fcfeff0c7d67c394e9a42964dbb41ec6d6
GEOVANA CRISTINA VALENTIM BUENO <0200832311021>
2024-09-12 12:19:36 -0300

    correção de erros

--- src/game.js
+++ src/game.js
@@ -1,11 +1,13 @@
 export const game = {
     letters: [],
     word: "",
+    streak: 0,
+    inputedWords:[], 
     error: false,
     newGame: function(){
         this.letters = [];
 
-        const alphabet = "abcdefghijklmnopkrstuvwxyz";
+        const alphabet = "aabcdefghiijlmoooprstuuvz";
 
         //const letter = alphabet[randomIndex];
         
@@ -16,6 +18,31 @@ export const game = {
 
         this.word = "";
         this.error = false;
+        this.streak = 0;
 
+    },
+    validateWord: async function(word){
+        const tempLetters = [...this.letters];
+        for (let i = 0; i < this.letters.length; i++){
+            if(!word.includes(this.letters[i])){
+                this.error = true;
+                return false;
+            }
+        }
+
+        if(this.inputedWords.includes(word)){
+            return false;
+        }
+
+        const rawData = await fetch(`https://api.dicionario-aberto.net/word/${word}`);
+        const data = await rawData.json();
+
+        if (!data.length) {
+            return false;
+        }
+        
+        this.inputedWords.push(word);
+        this.streak++;
+        return true;
     }
 }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import { game } from "./game";
 
 const newGameButtonElem = document.getElementById('new-game');
-newGameButtonElem.addEventListener('click', function() {
+const inputElem = document.getElementById('word');
+const errorElem = document.getElementById('error');
+const streakElem = document.getElementById('streak')
+
+newGameButtonElem.addEventListener('click', function () {
+
     game.newGame();
 
     const lettersElem = document.querySelectorAll('.square');
@@ -10,4 +15,24 @@ newGameButtonElem.addEventListener('click', function() {
         lettersElem[i].innerHTML = game.letters[i].toUpperCase();
     }
 
+    inputElem.disabled = false;
+    inputElem.value = '';
+    inputElem.focus();
+    errorElem.style.visibility = 'hidden';
+    streakElem.innerHTML = '';
+})
+
+document.getElementById('form').addEventListener('submit', async function(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const word = formData.get('word');
+
+    const isWordValid = await game.validateWord(word);
+
+    if (isWordValid){
+        streakElem.innerHTML = game.streak;
+    } else {
+        errorElem.style.visibility = "visible";
+    }
 })
